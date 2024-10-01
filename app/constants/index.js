6e89ae21a469a053e999ff961b4db851e9022005
Rohan Patel <rohandvp@gmail.com>
2024-09-30 22:02:19 -0400

    Added Software Management

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,28 +1,36 @@
 export const EXPERIENCES = [
-	{
-		year: "Jun 2024 - Present",
-		role: "Software Engineer Intern",
-		company: "R1649 Private Equity Partners",
-		description: `Led a team migrating front-end codebase from React to Next.js, cleaning up the user interface, resulted in 16% increase in runtime efficiency. Assisted in full stack integration, integrating a python database into the front-end.`,
-		technologies: ["Next.js", "TailwindCSS", "React", "Python"],
-	},
-	{
-		year: "Jun 2023 - Present",
-		role: "Facility Management",
-		company: "YMCA",
-		description: `Worked in a 5-member team overseeing maintenance in 5+ facilities across Charlotte. Decreased the frequency of maintenance requests by 26%.`,
-		technologies: [
-			"Problem Solving",
-			"Decision Making",
-			"Teamwork",
-			"Communication",
-		],
-	},
-	{
-		year: "Jun 2022 - Jul 2022",
-		role: "Intern",
-		company: "Bosch Rexroth",
-		description: `Streamlined logistics within the warehouse by utilizing 5S to create a new flow model resulting in a 13% increase in efficiency. Modeled and constructed a new storage system providing ease of access between two industrial laser engravers.`,
-		technologies: ["SolidWorks", "AutoCAD"],
-	},
+  {
+    year: "Aug 2024 - Present",
+    role: "Software Manager",
+    company: "R1649 Private Equity Partners",
+    description:
+      "Led AI initiatives to train models that parsed over 1000 data points for industry-specific insights, improving the company’s decision-making process. Managed the integration of AI- driven data extraction systems into the company’s website, providing realtime, relevant content for users. Spearheaded the development of machine learning workflows to customize data output optimizing business operations by finding over 250 potential investors.",
+    technologies: ["Python", "SuperAGI", "Scrapy", "Management", "Leadership"],
+  },
+  {
+    year: "Jun 2024 - Aug 2024",
+    role: "Lead Software Engineer",
+    company: "R1649 Private Equity Partners",
+    description: `Led a team migrating front-end codebase from React to Next.js, cleaning up the user interface, resulted in 16% increase in runtime efficiency. Assisted in full stack integration, integrating a python database into the front-end.`,
+    technologies: ["Next.js", "TailwindCSS", "React", "Python"],
+  },
+  {
+    year: "Jun 2023 - Aug 2024",
+    role: "Facility Management",
+    company: "YMCA",
+    description: `Worked in a 5-member team overseeing maintenance in 5+ facilities across Charlotte. Decreased the frequency of maintenance requests by 26%.`,
+    technologies: [
+      "Problem Solving",
+      "Decision Making",
+      "Teamwork",
+      "Communication",
+    ],
+  },
+  {
+    year: "Jun 2022 - Jul 2022",
+    role: "Intern",
+    company: "Bosch Rexroth",
+    description: `Streamlined logistics within the warehouse by utilizing 5S to create a new flow model resulting in a 13% increase in efficiency. Modeled and constructed a new storage system providing ease of access between two industrial laser engravers.`,
+    technologies: ["SolidWorks", "AutoCAD"],
+  },
 ];
